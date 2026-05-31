6df7ecc2b55bee075df588e38ddae816746e0bcd
Dave Patrick I. Bulaso <158995199+patrickpatrick27@users.noreply.github.com>
2026-06-01 00:37:28 +0800

    Fine-tune profile photo crop position

--- components/About.tsx
+++ components/About.tsx
@@ -38,7 +38,7 @@ export default function About() {
                 alt="Dave Patrick Bulaso"
                 width={96}
                 height={96}
-                className="object-cover w-full h-full" style={{ objectPosition: "center 30%" }}
+                className="object-cover w-full h-full" style={{ objectPosition: "center 8%" }}
               />
             </div>
           </div>
